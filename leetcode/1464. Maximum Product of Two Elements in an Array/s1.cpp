d0e5dd353f4c2b3f149f32e4c8e57d7995a7eefd
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-04-25 15:28:48 +0800

    1464

---
+++ leetcode/1464. Maximum Product of Two Elements in an Array/s1.cpp
@@ -0,0 +1,11 @@
+// OJ: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return (nums[nums.size()-1] - 1) * (nums[nums.size()-2] - 1); 
+    }
+};
