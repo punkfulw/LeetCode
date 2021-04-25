55e461359155b2791ec36696a90016ee7762bdf2
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-04-25 15:29:39 +0800

    1464

--- leetcode/1464. Maximum Product of Two Elements in an Array/s1.cpp
+++ leetcode/1464. Maximum Product of Two Elements in an Array/s1.cpp
@@ -1,6 +1,6 @@
 // OJ: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
 // Author: github.com/punkfulw
-// Time: O(N)
+// Time: O(NlogN)
 // Space: O(1)
 class Solution {
 public:
