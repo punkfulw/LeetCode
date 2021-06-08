beeb53f4eaa5f117b46935b48abf72d478bb741e
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-06-08 10:04:38 +0800

    412

---
+++ leetcode/412. Fizz Buzz/s2.cpp
@@ -0,0 +1,19 @@
+// OJ: https://leetcode.com/problems/fizz-buzz/
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans (n);
+        for (int i = 1; i <= n; i++)
+            ans[i-1] = to_string(i);
+        for (int i = 2; i < n; i += 3)
+            ans[i] = "Fizz";
+        for (int i = 4; i < n; i += 5)
+            ans[i] = "Buzz";
+        for (int i = 14; i < n; i += 15)
+            ans[i] = "FizzBuzz";
+        return ans;
+    }
+};
