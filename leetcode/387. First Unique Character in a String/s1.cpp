3a2558b866c566ce5e0c7202d549c0a524bf1b99
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-07-15 11:16:18 +0800

    387

---
+++ leetcode/387. First Unique Character in a String/s1.cpp
@@ -0,0 +1,16 @@
+// OJ: https://leetcode.com/problems/first-unique-character-in-a-string/
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int cnt[26] = {0};
+        for (char c: s)
+            cnt[c - 'a']++;
+        for (int i = 0; i < s.size(); i++)
+            if (cnt[s[i] - 'a'] == 1)
+                return i;
+        return -1;
+    }
+};
