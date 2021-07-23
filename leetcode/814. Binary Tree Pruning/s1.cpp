c97e68ca50c5001dbfdea284b34bb9ffd475a878
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-07-23 21:38:23 +0800

    814

---
+++ leetcode/814. Binary Tree Pruning/s1.cpp
@@ -0,0 +1,14 @@
+// OJ: https://leetcode.com/problems/binary-tree-pruning/
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(logN)
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (!root)
+            return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        return !root->left && !root->right && !root->val ? NULL : root;
+    }
+};
