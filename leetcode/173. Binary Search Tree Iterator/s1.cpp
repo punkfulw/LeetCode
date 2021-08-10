bf6758ac54caa8c25997e7925c70b5d6ca31ec79
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-08-10 20:09:29 +0800

    173

---
+++ leetcode/173. Binary Search Tree Iterator/s1.cpp
@@ -0,0 +1,30 @@
+// OJ: https://leetcode.com/problems/binary-search-tree-iterator/
+// Author: github.com/punkfulw
+// Ref: https://leetcode.com/problems/binary-search-tree-iterator/discuss/52525/My-solutions-in-3-languages-with-Stack
+// Time: O(1) 
+// Space: O(H)
+class BSTIterator {
+public:
+    stack<TreeNode*> stk;
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        auto cur = stk.top();
+        stk.pop();
+        pushAll(cur->right);
+        return cur->val;
+    }
+    
+    bool hasNext() {
+        return !stk.empty();
+    }
+    void pushAll(TreeNode* root){
+        while (root){
+            stk.push(root);
+            root = root->left;
+        }
+        return;
+    }
+};
