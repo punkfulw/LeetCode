41b84c3a0547071f2598fd1a57f846d66e0c291f
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-08-18 11:03:04 +0800

    119

---
+++ leetcode/1190. Reverse Substrings Between Each Pair of Parentheses/s2.cpp
@@ -0,0 +1,25 @@
+// OJ: https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+// Author: github.com/punkfulw
+// Time: O(N^2)
+// Space: O(N)
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> stk;
+        string ans;
+
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == '(')
+                stk.push(i);
+            else if (s[i] == ')'){
+                int top = stk.top();
+                reverse(s.begin() + top, s.begin() + i);
+                stk.pop();
+            }
+        }
+        for (auto c: s)
+            if (c != '(' && c != ')')
+                ans += c;
+        return ans;
+    }
+};
