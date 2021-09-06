43a6b44bc0dc241e9849ff82d070692edba030ec
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-09-06 12:12:00 +0800

    1984

---
+++ leetcode/1984. Minimum Difference Between Highest and Lowest of K Scores/s1.py
@@ -0,0 +1,11 @@
+// OJ: https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+// Author: github.com/punkfulw
+// Time: O(NlogN)
+// Space: O(1)
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = sys.maxsize
+        for i in range(len(nums) - k + 1):
+            ans = min(ans, nums[i + k - 1] - nums[i])
+        return ans
