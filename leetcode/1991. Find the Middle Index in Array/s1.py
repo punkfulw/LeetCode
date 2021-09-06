d0af9c3c5f762cce1182991f2fadbe1ae6a1e11d
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-09-06 10:09:38 +0800

    1991

---
+++ leetcode/1991. Find the Middle Index in Array/s1.py
@@ -0,0 +1,17 @@
+// OJ: https://leetcode.com/problems/find-the-middle-index-in-array/
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(1)
+class Solution(object):
+    def findMiddleIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, sum(nums)
+        for i, num in enumerate(nums):
+            right -= num;
+            if right == left:
+                return i
+            left += num
+        return -1
