66fa247595719c4b8294b7fac07e0d760cb0a76a
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-09-13 11:04:07 +0800

    Create s2.py

---
+++ leetcode/53. Maximum Subarray/s2.py
@@ -0,0 +1,11 @@
+# OJ: https://leetcode.com/problems/maximum-subarray/
+# Author: github.com/punkfulw
+# Time: O(N)
+# Space: O(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans = cur = nums[0]
+        for i in range(1, len(nums)):
+            cur = max(nums[i], cur + nums[i])
+            ans = max(ans, cur)
+        return ans
