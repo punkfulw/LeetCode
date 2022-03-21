c901bdae362a1437d2a95ca9159cb0de527e061d
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-03-21 18:32:27 +0800

    1476

---
+++ leetcode/1476. Subrectangle Queries/s2.cpp
@@ -0,0 +1,24 @@
+// OJ: https://leetcode.com/problems/subrectangle-queries/
+// Author: github.com/punkfulw
+// Ref: https://github.com/lzl124631x/LeetCode/edit/master/leetcode/1476.%20Subrectangle%20Queries/README.md 
+// Time:
+//      O(MN) for SubrectangleQueries
+//      O(1) for updateSubrectangle
+//      O(Update) for getValue
+// Space: O(MN)
+class SubrectangleQueries {
+    vector<vector<int>> A, sub;
+public:
+    SubrectangleQueries(vector<vector<int>>& rectangle) : A(rectangle) {}
+    
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        sub.push_back({ row1, col1, row2, col2, newValue });
+    }
+    
+    int getValue(int row, int col) {
+        for (int i = sub.size() - 1; i >= 0; --i) {
+            if (row >= sub[i][0] && row <= sub[i][2] && col >= sub[i][1] && col <= sub[i][3]) return sub[i][4];
+        }
+        return A[row][col];
+    }
+};
