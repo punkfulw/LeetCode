c77adf6afb5c899b01109696be4602fca1b5981a
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-06-01 08:38:42 +0800

    1480

---
+++ leetcode/1480. Running Sum of 1d Array/s2.cpp
@@ -0,0 +1,12 @@
+// OJ: https://leetcode.com/problems/running-sum-of-1d-array/
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++)
+            nums[i] += nums[i - 1];
+        return nums;
+    }
+};
