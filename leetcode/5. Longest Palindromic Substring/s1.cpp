bf0c31e437b39f7738d26f8f3efd53ddd70352aa
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-06-16 08:53:56 +0800

    5

--- leetcode/5. Longest Palindromic Substring/s1.cpp
+++ leetcode/5. Longest Palindromic Substring/s1.cpp
@@ -1,31 +1,38 @@
 // OJ: https://leetcode.com/problems/longest-palindromic-substring/
 // Author: github.com/punkfulw
-// Reference: https://leetcode.com/problems/longest-palindromic-substring/discuss/2923/Simple-C%2B%2B-solution-(8ms-13-lines)
-// Time: O(N)
+// Time: O(N^2)
 // Space: O(1)
 class Solution {
 public:
     string longestPalindrome(string s) {
+        int mx = 0, n = s.size();
         string ans;
-        int n = s.size(), i = 0;
-        int start_index, longest = 0;
-        if (s.size() <= 1)
-            return s;
-        while (i < n){
-            int st = i, ed = i;
-            while (s[ed] == s[ed+1] && ed < n-1)
-                ed++;
-            i = ed+1;
-            while (st > 0 && ed < n - 1 && s[st-1] == s[ed+1]){
-                st--;
-                ed++;
+        
+        for (int i = 0; i < n; i++){
+            int l = i, r = i + 1;
+            while (l >= 0 && r < n){
+                if (s[l] == s[r])
+                    l--, r++;
+                else
+                    break;
             }
-            int cur = ed - st + 1;
-            if (cur > longest){
-                start_index = st;
-                longest = cur;
+            if (r - l - 1 > mx){
+                mx = r - l - 1;
+                ans = s.substr(l + 1, r - l - 1);
+            }
+            l = i - 1, r = i + 1;
+            while (l >= 0 && r < n){
+                if (s[l] == s[r])
+                    l--, r++;
+                else
+                    break;
+            }
+            if (r - l - 1 > mx){
+                mx = r - l - 1;
+                ans = s.substr(l + 1, r - l - 1);
             }
         }
-        return s.substr(start_index, longest);
+        return ans;
+        
     }
 };
