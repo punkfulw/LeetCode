2b00a11954a2391efb1358d572ee52b902386e08
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-07-06 22:56:48 +0800

    50

---
+++ leetcode/504. Base 7/s2.cpp
@@ -0,0 +1,14 @@
+// OJ: https://leetcode.com/problems/base-7/
+// Author: github.com/punkfulw
+// Time: O(logN)
+// Space: O(logN)
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if (num < 0)
+            return "-" + convertToBase7(-num);
+        if (num < 7)
+            return to_string(num);
+        return convertToBase7(num / 7) + convertToBase7(num % 7);
+    }
+};
