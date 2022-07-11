dda9f1ab155129fdb60e7f17816900ac06af06b9
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-07-11 19:40:07 +0800

    103

---
+++ leetcode/103. Binary Tree Zigzag Level Order Traversal/s1.cpp
@@ -0,0 +1,31 @@
+// OJ: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(N)
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<vector<int>> ans;
+        if (!root)
+            return ans;
+        q.push(root);
+        int cnt = 0;
+        while (!q.empty()){
+            int n = q.size();
+            ans.push_back({});
+            for (int i = 0; i < n; i++){
+                auto node = q.front(); q.pop();
+                ans.back().push_back(node->val);
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            if (cnt % 2)
+                reverse(ans.back().begin(), ans.back().end());
+            cnt++;
+        }
+        return ans;
+    }
+};
