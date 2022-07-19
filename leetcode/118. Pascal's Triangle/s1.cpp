6119853124262cc819064ddc41571f397989c0ce
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-07-19 08:12:39 +0800

    118

--- leetcode/118. Pascal's Triangle/s1.cpp
+++ leetcode/118. Pascal's Triangle/s1.cpp
@@ -1,18 +1,17 @@
 // OJ: https://leetcode.com/problems/pascals-triangle/
 // Author: github.com/punkfulw
-// Reference: https://leetcode.com/problems/pascals-triangle/discuss/38171/Maybe-shortest-c%2B%2B-solution
 // Time: O(N^2)
 // Space: O(1)
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans(numRows);
-        
-        for (int i = 0; i < numRows; i++){
-            ans[i].resize(i+1);
-            ans[i][0] = ans[i][i] = 1;
-            for (int j = 1; j < i; j++)
-                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+    vector<vector<int>> generate(int R) {
+        vector<vector<int>> ans;
+        for (int i = 1; i <= R; i++){
+            ans.push_back(vector<int> (i, 1));
+            if (i <= 2) continue;
+            for (int j = 1; j < i - 1; j++){
+                ans[i - 1][j] = ans[i - 2][j - 1] + ans[i - 2][j];
+            }
         }
         return ans;
     }
