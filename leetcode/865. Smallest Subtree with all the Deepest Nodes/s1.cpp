c71f67e5d9c23e0869d6cc29709f495d8d0212ac
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-08-08 21:08:14 +0800

    865

---
+++ leetcode/865. Smallest Subtree with all the Deepest Nodes/s1.cpp
@@ -0,0 +1,32 @@
+// OJ: https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+// Author: github.com/punkfulw
+// Time: O(N)
+// Space: O(H)
+class Solution {
+public:
+    int depth = 0;
+    TreeNode* ans;
+    int dfs(TreeNode* root){
+        if (!root)
+            return 0;
+        return 1 + max(dfs(root->left), dfs(root->right));
+    }
+    
+    int sol(TreeNode* root, int cnt){
+        if (!root)
+            return 0;
+        if (cnt == depth){
+            ans = root;
+            return 1;
+        }
+        int l = sol(root->left, cnt + 1), r = sol(root->right, cnt + 1);
+        if (l && r)
+            ans = root;
+        return l || r;
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        depth = dfs(root) - 1;
+        sol(root, 0);
+        return ans;
+    }
+};
