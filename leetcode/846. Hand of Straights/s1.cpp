a861ce5d0c437cba5edf85e8947ce27bdeac5b33
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-11-09 14:24:32 +0800

    846

---
+++ leetcode/846. Hand of Straights/s1.cpp
@@ -0,0 +1,23 @@
+// OJ: https://leetcode.com/problems/hand-of-straights/
+// Author: github.com/punkfulw
+// Time: O(NlogN)
+// Space: O(N)
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& nums, int k) {
+        map<int, int> mp;
+        
+        for (auto &i: nums) mp[i]++;
+        
+        for (auto [first, num]: mp){
+            if (num == 0)
+                continue;
+            for (int i = first; i < first + k; i++){
+                if (mp[i] < num)
+                    return false;
+                mp[i] -= num;
+            }
+        }
+        return true;
+    }
+};
