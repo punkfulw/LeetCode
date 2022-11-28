ac5e62dd913996f8c2bb0c984880b706859e0f8d
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-11-28 10:25:44 +0800

    791

--- leetcode/791. Custom Sort String/s1.cpp
+++ leetcode/791. Custom Sort String/s1.cpp
@@ -1,16 +1,18 @@
 // OJ: https://leetcode.com/problems/custom-sort-string/
 // Author: github.com/punkfulw
-// Ref: https://leetcode.com/problems/custom-sort-string/discuss/116556/Two-Lines-C%2B%2B
 // Time: O(NlogN)
 // Space: O(1)
 class Solution {
 public:
-    string customSortString(string order, string str) {
-        map<char, int> map;
-        for (int i = 0; i < order.size(); i++)
-            map[order[i]] = i+1;
+    string customSortString(string order, string s) {
+        unordered_map<char, int> mp;
         
-        sort(str.begin(), str.end(), [&](char a, char b) { return map[a] < map[b]; });
-        return str;
+        for (int i = 0; i < order.size(); i++)
+            mp[order[i]] = i;
+
+        sort(s.begin(), s.end(), [&](char &a, char &b){
+           return mp[a] < mp[b]; 
+        });
+        return s;
     }
 };
