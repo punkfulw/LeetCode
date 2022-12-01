9d9cf2aeba2b640042281f9f73ac1ef044466223
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-12-01 13:11:01 +0800

    833

---
+++ leetcode/833. Find And Replace in String/s1.cpp
@@ -0,0 +1,38 @@
+// OJ: https://leetcode.com/problems/find-and-replace-in-string/
+// Author: github.com/punkfulw
+// Time: O(N^N + KlogK)
+// Space: O(K)
+class Solution {
+public:
+    bool check(string &s, int idx, string &ss){
+        if (idx + ss.size() > s.size())
+            return false;
+        for (int i = 0; i < ss.size(); i++)
+            if (s[idx + i] != ss[i])
+                return false;
+        return true;
+    }
+    string findReplaceString(string s, vector<int>& idxs, vector<string>& srs, vector<string>& tars) {
+        int k = idxs.size();
+        string ans = "";
+        map<int, pair<string, string>> mp;
+        
+        for (int i = 0; i < k; i++){
+            int idx = idxs[i];
+            string ss = srs[i], tar = tars[i];
+            mp[-idx] = {ss, tar};
+        }
+        
+        for (auto p: mp){
+            int idx = -p.first;
+            string ss = p.second.first, tar = p.second.second;
+            // cout << idx << " " << ss << " " << tar << endl;
+            if (!check(s, idx, ss))
+                continue;
+            string head = s.substr(0, idx), tail = s.substr(idx + ss.size());
+            // cout << head << " " << tail << endl;
+            s = head + tar + tail;
+        } 
+        return s;
+    }
+};
